b8efd815b6a9223c34f17b335717121acfd6e0ed
Amin <mohamed.amin.088@gmail.com>
2016-06-17 04:46:46 +0400

    add better proxy for actions

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -3,23 +3,23 @@
  * Widget Controller is the main Controller
  */
 angular.module('myApp').controller('HomeCtrl', function($scope, Api) {
-    $scope.text = 'hi';
-
     $scope.properties = Api.getProperties() || [];
     $scope.savedProperties = Api.getSavedProperties() || [];
     $scope.propertyAction = (options) => {
         options = options || {};
-        let isSaving = options.action === 'SAVE';
-        let source = isSaving ? 'properties' : 'savedProperties';
-        let target = isSaving ? 'savedProperties' : 'properties';
+        let actionTypes = {
+            'SAVE': { target: 'savedProperties', source: 'properties' },
+            'REMOVE': { target: 'properties', source: 'savedProperties' },
+        }
+
+        let source = actionTypes[options.action].source;
+        let target = actionTypes[options.action].target;
         let property = _.find($scope[source], { id: options.id });
 
         $scope[target].push(property);
-        $scope[source] = _.filter($scope[source], (p)=>{
-        	return p.id !== options.id;
+        $scope[source] = _.filter($scope[source], (p) => {
+            return p.id !== options.id;
         })
     }
-
-
 
 });
